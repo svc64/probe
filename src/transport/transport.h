6d161b494326bb6b1ba9e39b1bac591a46e816a1
svc64 <svcx80@icloud.com>
2023-01-20 13:15:20 +0200

    conn -> req

--- src/transport/transport.h
+++ src/transport/transport.h
@@ -10,7 +10,7 @@ int probe_transport_init();
 probe_req_t *probe_transport_recv(void **data, uint64_t *len);
 
 // Send a response. "conn" is free'd.
-int probe_transport_send(probe_req_t *conn, void *data, uint64_t len);
+int probe_transport_send(probe_req_t *req, void *data, uint64_t len);
 
 // Destroy the transport.
 void probe_transport_destroy();
