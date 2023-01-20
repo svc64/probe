f5137786662edf7ce731c86f3eb94c415f923f47
svc64 <svcx80@icloud.com>
2023-01-20 18:33:36 +0200

    nop command

--- probe_client/client.py
+++ probe_client/client.py
@@ -5,8 +5,13 @@ class ProbeReplyException(Exception):
 
 class ProbeClient:
 
+    OP_NOP = 0
+
     def __init__(self, transport):
         self.transport = transport
 
     def request(self, opcode, *args):
         return self.transport.request(opcode, *args)
+
+    def nop(self):
+        return self.request(self.OP_NOP)
