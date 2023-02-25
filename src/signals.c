a9bc7b0d46f4ce13416caa1f1f1b37d7070c117e
svc64 <svcx80@icloud.com>
2023-02-25 11:42:57 +0200

    Clarity use of the signal handler

--- src/signals.c
+++ src/signals.c
@@ -5,30 +5,30 @@
 #include <pthread.h>
 #include "requests.h"
 
-#define MEM_THREAD_NAME "svc.probe_thread"
+#define SIGNAL_THREAD_NAME "svc.probe_thread"
 
 int set_probe_thread()
 {
     int err;
 #ifdef __APPLE__
-    err = pthread_setname_np(MEM_THREAD_NAME);
+    err = pthread_setname_np(SIGNAL_THREAD_NAME);
 #else
-    err = pthread_setname_np(pthread_self(), MEM_THREAD_NAME);
+    err = pthread_setname_np(pthread_self(), SIGNAL_THREAD_NAME);
 #endif
     return err;
 }
 
-/* The client can perform memory r/w operations that might result in a fault. To deal with this,
-when we do such operations, we do them on a new thread. The new thread's name is set
-to MEM_THREAD_NAME. When a fault occurs, the signal handler will run *in that thread*.
-Then, we check the thread's name to see if it's MEM_THREAD_NAME. If it is, we just use pthread_exit
+/* The client can perform memory r/w operations and syscalls that might result in a signal (SIGSEGV / SIGSYS).
+To deal with this, when we do such operations, we do them on a new thread. The new thread's name is set
+to SIGNAL_THREAD_NAME. When we get a signal, the signal handler will run *in that thread*.
+Then, we check the thread's name to see if it's SIGNAL_THREAD_NAME. If it is, we just use pthread_exit
 to make the thread exit (and return STATUS_FAULT). */
 void sighandler(int signo, siginfo_t *si, void *data)
 {
     char thread_name[0xFF];
     bzero(thread_name, sizeof(thread_name));
     pthread_getname_np(pthread_self(), thread_name, sizeof(thread_name));
-    if (!strcmp(thread_name, MEM_THREAD_NAME)) {
+    if (!strcmp(thread_name, SIGNAL_THREAD_NAME)) {
         pthread_exit((void *)STATUS_FAULT);
     }
     printf("unhandled signal!\n");
