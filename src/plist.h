87392f32fe00e043575b0584f3a5f50f03d2701f
svc64 <svcx80@icloud.com>
2023-02-28 22:46:54 +0200

    bye plist mess

--- src/plist.h
+++ src/plist.h
@@ -1,5 +1,9 @@
 #include <stdbool.h>
 #include "mini_libplist/libplist.h"
 
+#define PLIST_KEY_STATUS "status"
+#define PLIST_KEY_PAYLOAD "payload"
+#define PLIST_KEY_OP "op"
+
 bool plist_array_get_item_type(plist_t node, uint32_t index, plist_type type, plist_t *item_out);
 bool plist_array_get_int(plist_t node, uint32_t index, uint64_t *result);
--- src/probe_plist.h
+++
@@ -1,5 +0,0 @@
-#include "plist.h"
-
-#define PLIST_KEY_STATUS "status"
-#define PLIST_KEY_PAYLOAD "payload"
-#define PLIST_KEY_OP "op"
